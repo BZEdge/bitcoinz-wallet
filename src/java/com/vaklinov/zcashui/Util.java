eb4159bd66a5e580559ff6f10e50378f21bc575b
Miodrag Popović <miodragpopovic@stcable.net>
2019-03-21 08:30:22 +0100

    all addresses starting with 'z' are considered private

--- src/java/com/vaklinov/zcashui/Util.java
+++ src/java/com/vaklinov/zcashui/Util.java
@@ -355,7 +355,7 @@ public class Util
 	public static boolean isZAddress(String address)
 	{
 		return (address != null) && 
-			   (address.startsWith("zc") || address.startsWith("zt")) && 
+			   (address.startsWith("z")) && 
 			   (address.length() > 40);
 	}
 	
